a7d759745e2e4525a1dbf75b7c28ff211a0cf787
StorozhenkoSvyatoslav <svyatoslavstorozhenko@gmail.com>
2025-11-17 13:00:11 +0300

    Delete NewsApp-master directory

--- NewsApp-master/app/src/main/java/com/example/newsapp/navigation/Screen.kt
+++
@@ -1,7 +0,0 @@
-package com.example.newsapp.navigation
-
-sealed class Screen(val route: String) {
-    object Home : Screen("Home")
-    object Search : Screen("Search")
-    object Favorites : Screen("Favorites")
-}
